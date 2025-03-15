268dd005f22ffde65bcf863a26d690c8c47bae51
Joaaquim1801 <joaquimaiafilho2018@gmail.com>
2025-03-14 22:03:16 -0300

    Version 01

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1:{'nome': 'Crab Nebula',
+    'legenda': 'webbtelescope.org / NASA / James Webb'},
+    2:{'nome': 'Arp 220 Galaxy',
+    'legenda': 'webbtelescope.org / NASA / James Webb'}
+    }
+    return render(request, 'galeria/index.html', {"cards": dados })
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
