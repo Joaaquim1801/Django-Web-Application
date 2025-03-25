770c0e0e05563df67be4902aa22026c80ecbda13
Joaaquim1801 <joaquimaiafilho2018@gmail.com>
2025-03-25 11:39:00 -0300

    Version 03

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from galeria.models import Fotografias
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicado")
     list_display_links = ("id","nome")
-    search_fields = ("nome","categoria") #Precisa ser uma tupla
+    search_fields = ("nome",) #Precisa ser uma tupla
+    list_filter = ("categoria",)
+    list_per_page = 10
+    list_editable = ("publicado",)
+
 
 admin.site.register(Fotografias, ListandoFotografias)
--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografias
 
 def index(request):
-    fotografias = Fotografias.objects.all() # Vai acessar os objetos no banco de dados
+    fotografias = Fotografias.objects.order_by("data_fotografia").filter(publicado=True) #O all() Vai acessar os objetos no banco de dados
+    #já o filter vai filtrar aqueles que possuem a característica mencionada
+    #order_by() ele vai ordenar os itens já cadatrados com base em uma informação
+    #-data_fotografia = ordenar pelo mais antigo, o "-" tem a função de tipo fazer o inverso
     return render(request, 'galeria/index.html', {"cards": fotografias })
 
 def imagem(request, foto_id):
